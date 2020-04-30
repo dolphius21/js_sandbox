fbe4cffe2351c2f22858cfbf6f01fca5c85a65b1
dolphius21 <johnrudolphlee@gmail.com>
2020-04-30 23:03:49 +0800

    added lesson 3

---
+++ Lesson_3_Data_Types/app.js
@@ -0,0 +1,39 @@
+// Primitive
+
+// String
+const name = 'John Doe';
+
+// Number
+const age = 45;
+
+//Boolean
+const hasKids = true;
+
+//Null
+const car = null;
+
+// Undefined
+let test;
+
+// Symbol
+const sym = Symbol();
+
+console.log(typeof sym);
+
+
+// Reference - typeof = object
+
+// Array
+const hobbies = ['movies', 'music'];
+
+// Object Literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
+}
+
+// Date
+const today = new Date()
+
+console.log(today);
+console.log(typeof today);
