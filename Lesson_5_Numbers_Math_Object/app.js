1c9949cf3207c5b8c18c00014a0a31905943b6b0
dolphius21 <johnrudolphlee@gmail.com>
2020-05-01 11:09:45 +0800

    added lesson 5

---
+++ Lesson_5_Numbers_Math_Object/app.js
@@ -0,0 +1,26 @@
+const num1 = 100;
+const num2 = 60;
+let val;
+
+// simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(Math.PI);
+val = Math.floor(Math.PI);
+val = Math.sqrt(64);
+val = Math.abs(-4);
+val = Math.pow(8, 2);
+val = Math.min(9,21,22,11,-12,64,-58);
+val = Math.max(9,21,22,11,-12,64,-58);
+val = Math.random();
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
