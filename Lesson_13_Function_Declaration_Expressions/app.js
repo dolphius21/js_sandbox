468259f8a515c656ced95dabfe3cfc4485dcc80e
dolphius21 <johnrudolphlee@gmail.com>
2020-05-02 10:20:38 +0800

    added lesson 13

---
+++ Lesson_13_Function_Declaration_Expressions/app.js
@@ -0,0 +1,48 @@
+// Function Declarations
+
+function greet(firstName = 'John', lastName = 'Doe') {
+  // console.log('Hello');
+  return `Hello ${firstName} ${lastName}`;
+}
+
+console.log(greet('Steve', 'Rogers'));
+
+
+// Function Expressions
+
+const square = function(x = 3) {
+  return x * x;
+}
+
+console.log(square());
+
+
+// Immediately Invokable Function Expressions - IIFEs
+
+// (function(){
+//   console.log('IIFE Run..');
+// })()
+
+(function(name){
+  console.log('Hello ' + name);
+})('Steve')
+
+
+// Property Methods
+
+const todo = {
+  add: function () {
+    console.log('Add todo..');
+  },
+  edit: function (id) {
+    console.log(`Edit todo ${id}`);
+  }
+}
+
+todo.delete = function() {
+  console.log('Delete todo...');
+}
+
+todo.add();
+todo.edit(22);
+todo.delete();
