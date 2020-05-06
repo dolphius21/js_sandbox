37deee98084ae38c0fe1b1209531e867b76364c2
dolphius21 <johnrudolphlee@gmail.com>
2020-05-06 16:42:54 +0800

    Added lesson 15

---
+++ Lesson_15_Window_Object/app.js
@@ -0,0 +1,30 @@
+// Window Methods / Objects / Properties
+
+// Alert
+// alert('Hello World');
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if (confirm('Are you sure')) {
+//   console.log('Yes');
+// } else {
+//   console.log('No');
+// }
+
+let val;
+
+// Outer height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+// Inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll points
+val = window.scrollY
+
+console.log(val);
