eaff7abb65d45d433107a97e48f353af165aa314
dolphius21 <johnrudolphlee@gmail.com>
2020-05-17 16:18:02 +0800

    added fetch API

--- asynchronous_javascript/js/promises.js
+++ asynchronous_javascript/js/promises.js
@@ -3,30 +3,18 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
-function getJSON(url) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = () => {
-      if(xhr.status === 200) {
-        let data = JSON.parse(xhr.responseText);
-        resolve(data);
-      } else {
-        reject( Error(xhr.statusText) );
-      }
-    };
-    xhr.onerror = () => reject( Error('A network error occured') );
-    xhr.send();
-  });
-}
-
 function getProfiles(json) {
   const profiles = json.people.map(person => {
-
+    const craft = person.craft;
     if (person.name == "Anatoly Ivanishin") {
         person.name = "Anatoli_Ivanishin"
     }
-    return getJSON(wikiUrl + person.name);
+    return fetch(wikiUrl + person.name)
+        .then( response => response.json() )
+        .then( profile => {
+          return { ...profile, craft };
+        })
+        .catch( err => console.log('Error Fetching Wiki: ', err) )
    });
    return Promise.all(profiles);
 }
@@ -38,6 +26,7 @@ function generateHTML(data) {
     if (person.type === "standard") {
       section.innerHTML = `
       <img src=${person.thumbnail.source}>
+      <span>${person.craft}</span>
       <h2>${person.title}</h2>
       <p>${person.description}</p>
       <p>${person.extract}</p>`;
@@ -50,9 +39,14 @@ function generateHTML(data) {
 }
 
 btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl)
+  event.target.textContent = 'Loading...';
+  fetch(astrosUrl)
+  .then( response => response.json() )
     .then(getProfiles)
     .then( generateHTML )
-    .catch( err => console.log(err) )
-  event.target.remove();
+    .catch( err => {
+      peopleList.innerHTML = '<h3>Something went wrong!</h3>'
+      console.log(err)
+    })
+    .finally( () => event.target.remove() )
 });
