fbc5e7f089eefb3202abef79fbdd6a03082106c1
dolphius21 <johnrudolphlee@gmail.com>
2020-05-19 17:14:55 +0800

    practiced using object oriented javascript

--- object_oriented_javascript/pet.js
+++ object_oriented_javascript/pet.js
@@ -6,22 +6,49 @@ class Pet {
     this.sound =sound;
   }
 
+  get activity() {
+    const today = new Date();
+    const hour = today.getHours();
+    if (hour > 8 && hour <= 20) {
+      return 'playing';
+    } else {
+      return 'sleeping';
+    }
+  }
+  
+  get owner() {
+    return this._owner;
+  }
+
+  set owner(owner) {
+    this._owner = owner;
+  }
+
   speak() {
     console.log(this.sound);
   }
 }
 
+class Owner {
+  constructor(name, address) {
+    this.name = name;
+    this.address = address;
+  }
+
+  set phone(phone) {
+    const phoneNormalized = phone.replace(/[^0-9]/g, '');
+    this._phone = phoneNormalized;
+  }
+
+  get phone() {
+    return this._phone;
+  }
+}
+
 const ernie = new Pet('dog', 1, 'pug', 'arf arf');
-const vera = new Pet('dog', 8, 'border collie', 'woof woof');
-const scofield = new Pet('dog', 6, 'dobberman', 'bow wow');
-const edel = new Pet('dog', 7, 'german shorthaired pointer', 'aww aww');
+const vera = new Pet('dog', 1, 'border collie', 'woof woof');
 
-console.log(ernie);
-console.log(vera);
-console.log(scofield);
-console.log(edel);
+ernie.owner = new Owner('Ashley', '123 Main Street');
+ernie.owner.phone = '(555) 555-5555';
 
-ernie.speak();
-vera.speak();
-scofield.speak();
-edel.speak();
+console.log(ernie.owner);
